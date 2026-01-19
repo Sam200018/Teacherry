4930989d21461a776ec984b3fffbb00ea008b3f1
Samuel Bautista <samuelbaubas@gmail.com>
2026-01-19 13:24:46 -0600

    First commit

---
+++ shared/src/commonTest/kotlin/com/teacherry/SharedCommonTest.kt
@@ -0,0 +1,12 @@
+package com.teacherry
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
