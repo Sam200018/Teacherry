8a70e5fe46fe341c77a64c2a3ea7e26236ece8b6
Samuel Bautista <samuelbaubas@gmail.com>
2026-09-02 23:15:27 -0600

    CC3 | creating base config to new group feature

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -16,29 +16,17 @@ kotlin {
 
     sourceSets {
         androidMain.dependencies {
-            implementation(compose.preview)
-            implementation(libs.androidx.activity.compose)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            // Koin for DI
             implementation(project.dependencies.platform(libs.koin.bom))
-
-            // Koin modules (no versions needed)
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose.viewmodel)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.android)
-            implementation (libs.compose.material.icons)
+            implementation(libs.bundles.composeBasics)
+            implementation(libs.navigation.compose)
+            implementation(libs.bundles.viewModel)
+            implementation(libs.bundles.koin)
 
             implementation(project(":composeApp:features:login-registration"))
             implementation(project(":composeApp:features:home"))
+            implementation(project(":composeApp:features:groups"))
         }
         commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.ui)
-            implementation(compose.components.resources)
-            implementation(compose.preview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(projects.shared)
